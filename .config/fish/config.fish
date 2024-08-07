32f634b2d15b6d0d679c67d60b9b04637f5dcc89
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2024-08-06 20:19:06 -0700

    fix(brew): make brew work w/ fish on MacOS aarch64

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /usr/local/opt/asdf/libexec/asdf.fish
 alias todo='rg -t markdown --sort path -F -- "- [ ]" ~/notes/ ~/today.md'
+set brewcmd (path filter /opt/homebrew/bin/brew /usr/local/bin/brew)[1]
+and $brewcmd shellenv | source
+
+source ($brewcmd --prefix asdf)/libexec/asdf.fish
