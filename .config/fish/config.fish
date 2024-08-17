6d01f103ba3564240e2ffc7eeabbbfcbece36d22
Jonathan K. Wolski <jonathan.k.wolski@disney.com>
2024-08-17 14:55:53 -0700

    feat: new work machine

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,7 @@ set brewcmd (path filter /opt/homebrew/bin/brew /usr/local/bin/brew)[1]
 and $brewcmd shellenv | source
 
 source ($brewcmd --prefix asdf)/libexec/asdf.fish
+
+# Setting PATH for Python 3.9
+# The original version is saved in /Users/jonathan.k.wolski/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.9/bin" "$PATH"
